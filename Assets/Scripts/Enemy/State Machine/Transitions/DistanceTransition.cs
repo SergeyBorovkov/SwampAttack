3618d694a727c852276ef3b4bd6c981de62cdd6e
Sbor77 <sborovkov82@mail.ru>
2021-08-17 22:53:50 +0300

    Initial commit

---
+++ Assets/Scripts/Enemy/State Machine/Transitions/DistanceTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceTransition : Transition
+{
+    [SerializeField] private float _transitionRange;
+    [SerializeField] private float _transitionSpread;
+
+    private void Start()
+    {
+        _transitionRange += Random.Range(-_transitionSpread, _transitionSpread);
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, Target.transform.position) < _transitionRange)
+        {
+            NeedTransit = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon/AK47.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK47 : Weapon
+{
+    public override void Shoot(Transform shootPoint)
+    {
+        Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+    }
+}
